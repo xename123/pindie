0342b326e2feb231e68f4a2d7bab8d107d79389e
Timur Ignatenko <james.stat228l@gmail.com>
2024-05-21 17:12:17 +0900

    Финальное ревью 2

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindie-admin.nomoredomainswork.ru/";
+export const BASE_URL = "https://pindie-admin.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
