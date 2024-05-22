aa10a39f562eeda23b2438945eac3c7765ffc465
xename123 <133120961+xename123@users.noreply.github.com>
2024-05-22 19:15:54 +0900

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -2,6 +2,6 @@ export const BASE_URL = "https://pindie-admin.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
+  auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/users/me`,
 };
